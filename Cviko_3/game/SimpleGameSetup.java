2689fcdc5dd56e148a70601bc0c0a08ca7a0a324
Vadym Tilihuzov <71652431+Real-4339@users.noreply.github.com>
2022-03-03 21:53:57 +0100

    Add files via upload

---
+++ Cviko_3/game/SimpleGameSetup.java
@@ -0,0 +1,25 @@
+package fiit.cvicenia.Teachers_code.game;
+
+import fiit.cvicenia.Teachers_code.characters.*;
+
+public class SimpleGameSetup implements GameSetup {
+	public void setup(Knight[] knights, Ogre[] ogres, int nKnights, int nBraveKnights, int nBadOgres) {
+		int numberOfWarriors = nKnights + nBraveKnights;
+		
+		for (int i = 0; i < nKnights; ++i){
+			knights[i] = new Knight(80, new IronSword(i));
+		}
+
+		for (int i = nKnights; i < numberOfWarriors; ++i){
+			knights[i] = new BraveKnight(100, new LightSword(i));
+		}
+
+		for (int i = 0; i < nBadOgres; ++i){
+			ogres[i] = new BadOgre(100);
+		}
+		
+		for (int i = nBadOgres; i < numberOfWarriors; ++i){
+			ogres[i] = new Ogre(100);
+		}
+	}
+}
