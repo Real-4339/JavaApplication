bf3874a62721951bbf6f3aaee090274c7dd47a82
Vadym Tilihuzov <qvadym@stuba.sk>
2022-04-03 08:50:53 +0200

    setting up fxml and gui

---
+++ Project/ArtInstallationAuction/src/gui/log/LogginGui.java
@@ -0,0 +1,25 @@
+package gui.log;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class LogginGui extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println(getClass());
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login_form.fxml"));
+        stage.setTitle("User Login");
+        stage.setScene(new Scene(root, 800, 500));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
