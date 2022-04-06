fb0e66a558375ff09e5e7763ce1072edbc8ba0de
Vadym Tilihuzov <qvadym@stuba.sk>
2022-04-06 18:49:32 +0200

    Added comments to Storage class

--- Project/ArtInstallationAuction/src/independent/Storage.java
+++ Project/ArtInstallationAuction/src/independent/Storage.java
@@ -5,6 +5,7 @@ import database.SqlDB;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+// Class to storage user data
 public class Storage {
 	private SqlDB sql;
 	ArrayList<User> people = new ArrayList<User>();
@@ -21,15 +22,18 @@ public class Storage {
 		return people;
 	}
 	
+	// Fun to get users from DB
 	public void updateUsers () throws SQLException {
 		sql.User_out(people);
 	}
 	
+	// Fun to input a new User into DB
 	public void setArray (String login, String pass, String fname,String sname) throws SQLException {
 	  this.people.add(new User(login, pass, fname, sname, 0, 0));
 	  sql.User_in(people);
 	}
 	
+	// Fun to input a new User into DB, but also with Email
 	public void setArrayEmail (String login, String pass, String fname,String sname, String email) throws SQLException {
 		this.people.add(new User(login, pass, fname, sname, email, 0, 0));
 		 sql.User_in(people);
