f2d17df7e99490e3c0fd18ee882684f1d0620112
Vadym Tilihuzov <qvadym@stuba.sk>
2022-04-12 05:28:31 +0200

    Added buy func and lists for help

--- Project/ArtInstallationAuction/src/independent/Storage.java
+++ Project/ArtInstallationAuction/src/independent/Storage.java
@@ -1,15 +1,22 @@
 package independent;
 
 import database.SqlDB;
+import gui.main.MainGui;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 // Class to storage user data
 public class Storage {
 	private SqlDB sql;
+	private Product product = new Product("Mini liberty statue", 123, 0);
 	ArrayList<User> all = new ArrayList<User>();
 	ArrayList<User> active = new ArrayList<User>();
+	
+	private ArrayList<ArrayList> list = new ArrayList<>();
+	
+	public ArrayList getProducts(){return list;}
 	
 	public Storage () {
 		
@@ -78,6 +85,49 @@ public class Storage {
 		balance += money;
 		sql.User_updateBalance(man, balance, index);
 		updateUsers ();
+	}
+	
+	public String buy (String login, int money) throws SQLException {
+		product = new Product("Mini liberty statue", 123, product.getCount()+1);  
+		ArrayList list_01 = new ArrayList<>();
+		list_01.add(login);
+		list_01.add(money);
+		list.add(list_01);
+		if (product.getCount() >= 3) {
+			System.out.println("IN >=3 ");
+			int max = 0;
+			for(int a = 0; a < list.size(); a++) {
+			    for(int i = 0; i < 2; i++)
+			    {
+			    	if((int) list.get(a).get(1) > max) {
+			    		max = (int) list.get(a).get(1);
+			    	}
+			    }
+			}
+			 int persone = 0;
+			 String personne = "";
+			for(int a = 0; a < list.size(); a++) {
+			    for(int i = 0; i < 2; i++)
+			    {
+			    	if((int) list.get(a).get(1) == max) {
+			    		persone = a;
+			    		personne = (String) list.get(a).get(0);
+			    	}
+			    }
+			}
+			System.out.println("Persone: " + persone);
+			System.out.println("Personne: " + personne);
+			
+			String winner = personne + " Won";
+			for (int a = 0; a < active.size(); a++) {
+		        if (active.get(a).getLogin().equals(personne)) {
+		        	active.get(a).MinimizeBalance(max);
+		        }
+		    }
+			product.setCount(0);
+			return winner;
+		}
+		return "Waiting...";
 	}
 	
 }
